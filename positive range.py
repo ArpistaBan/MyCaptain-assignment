b73cbdd4e206dd670fa0f0238ce9856d6ede255d
ArpistaBan <87313166+ArpistaBan@users.noreply.github.com>
2021-07-12 18:03:59 +0530

    added positive range.py

---
+++ positive range.py
@@ -0,0 +1,14 @@
+#Write a program to print all positive numbers in range
+list1=[11,-5,67,-32,-21,4,14,-20]
+#iterating each number in list
+for num in list1:
+    #checking condition
+    if num>=0:
+        print(num, end=" ")
+
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
